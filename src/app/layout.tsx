ce0be97d0b9cc4217a7d61b288185d967490b2d7
moniruzzaman tasrif <moniruzzamantasif1@gmail.com>
2026-09-24 01:16:46 +0300

    add nav  bar area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navber from "@/component/Navber";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
+    data-theme="light"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+<Navber></Navber>
+        <main>
+        {children}
+        </main>
+
+        </body>
     </html>
   );
 }
---
+++ src/component/Navber.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import Logo from"../assets/logo.png"
+
+const Navber = () => {
+
+  const [ChaneColor,setChaneColor]=useState(false)
+
+  const handerClick=(dataget:string)=>{
+    if (dataget === "MyPlan"){setChaneColor(true)}else if (dataget === "Workouts") {
+      setChaneColor(false);
+    }
+
+  }
+
+  const NavLink = (
+    <>
+      <li>
+        {" "}
+        <Link
+          // className=" bg-[#1A2312] rounded-4xl px-6 text-[#C2F800]"
+          onClick={() => handerClick("Workouts")}
+          className={
+            ChaneColor
+              ? "text-gray-500"
+              : `bg-[#1A2312] rounded-4xl px-6 text-[#C2F800]`
+          }
+          href="/"
+        >
+          Workouts
+        </Link>
+      </li>
+      <li>
+        {" "}
+        <Link
+          href="/"
+          onClick={() => handerClick("MyPlan")}
+          className={
+            ChaneColor
+              ? `bg-[#1A2312] rounded-4xl px-6 text-[#C2F800]`
+              : "text-gray-500"
+          }
+        >
+          My Plan
+        </Link>
+      </li>
+    </>
+  );
+  return (
+    <div className="bg-[#0C0D10]">
+      <div className="navbar  shadow-sm max-w-350 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                aria-label="Menu"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {NavLink}
+            </ul>
+          </div>
+          <div className=" flex items-center gap-4">
+            <Image src={Logo} alt=""></Image>
+            <a className="  text-xl">FITLOG</a>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{NavLink}</ul>
+        </div>
+        <div className="navbar-end flex gap-4">
+          <Link href="/" className=" text-gray-300 flex gap-2">
+            {" "}
+            <span>Plan</span>
+            <span className=" px-2 rounded-4xl flex justify-center items-center text-white bg-[#C2F800]">
+              {" "}
+              0
+            </span>
+          </Link>
+          <Link href="/" className="text-gray-500 flex gap-2  ">
+            Saved
+            <span className="border px-2 rounded-4xl">0</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navber;
